07562d4585c748909c918efebd1b927bff00d9ca
torreswoo <dew1405@hanmail.net>
2015-03-18 01:43:10 +0900

    add Kmeans Thread

--- src/main/java/skplanet/crawling/App.java
+++ src/main/java/skplanet/crawling/App.java
@@ -23,6 +23,11 @@ public class App {
 		String url_courseInfo = "https://api.coursera.org/api/catalog.v1/courses?includes=categories&fields=name,shortDescription,shortName,language,targetAudience,smallIcon";
 		CourseCrawling course = new CourseCrawling(url_courseInfo);// course Info crawling
 		course.courseRun();  // 초기 정보수집
+		
+		//2. K means 알고리즘!
+		Kmeans kmeans = new Kmeans();
+		kmeans.start();
+				
 
 	}
 }
